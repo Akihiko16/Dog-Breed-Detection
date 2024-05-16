54779d8682fe64c496a3a291fc3d98f2438a3fe7
Reporin <72423521+Akihiko16@users.noreply.github.com>
2024-05-17 01:33:44 +0800

    Update app_dogbreedclassifier.py

--- app_dogbreedclassifier.py
+++ app_dogbreedclassifier.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 
 @st.cache_resource
 def load_model():
-  model=tf.keras.models.load_model('DogBreedClassifier.hdf5')
+  model=tf.keras.models.load_model('DogBreedClassifier.h5')
   return model
 model=load_model()
 st.write("""
